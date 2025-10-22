33809c8b9d51116ba54885a00a7a23a419e6ef4d
naimfurkansahin <naimfurkansahin@gmail.com>
2025-10-22 13:31:23 +0300

    OBS UYGULAMAMIZ OLUŞTURULDU

---
+++ program1.py
@@ -0,0 +1,14 @@
+'''
+yazar : nAİm
+mail: naimfurkansahin@gmail.com
+tarih: 22.10.2025
+
+açıklama: python programa dilini bir öğrenci
+bilgi sistemi senaryosu ile öğreten bir repo
+(anaprogram dosyası)
+
+'''
+
+if __name__ == "__main__":
+    print("\n----- ÖĞRENCİ BİLGİ SİSTEMİ -----")
+    

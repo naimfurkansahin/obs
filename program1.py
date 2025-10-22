b49bfa04cd1ae0c716875f85aea3e95d1dfe1ee0
naimfurkansahin <naimfurkansahin@gmail.com>
2025-10-22 14:33:05 +0300

    string format kullanmadan zorluklar

--- program1.py
+++ program1.py
@@ -10,5 +10,32 @@ bilgi sistemi senaryosu ile öğreten bir repo
 '''
 
 if __name__ == "__main__":
-    print("\n----- ÖĞRENCİ BİLGİ SİSTEMİ -----")
-    
+
+
+    baslik = "Öğrenci Bilgi Sistemi (v1)"
+
+    print(len(baslik))
+    print(baslik)
+
+    print("-"*100)
+    print("|",baslik," "*(94-(len(baslik)+1)),"|x|")
+    print("-"*100)
+
+    ogrenci_adi = input("Öğrenci adını giriniz: ")
+    ogrenci_soyadi = input("Öğrenci soyadını giriniz: ")
+    ogrenci_no = input("Öğrenci numarasını giriniz: ")
+
+    print("-"*100)
+    print("|","İsim"," "*(94-(len(baslik)+1)),"|x|")
+    print("-"*100)
+
+
+
+    '''
+    print("|"," "*96,"|")
+    print("|"," "*96,"|")
+    print("|"," "*96,"|")
+    print("|"," "*96,"|")
+    print("|"," "*96,"|")
+    print("|"," "*96,"|")
+    '''

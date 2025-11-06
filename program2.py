78cb9763252f04279428a09c2bf6cb5c1200d7e2
naimfurkansahin <naimfurkansahin@gmail.com>
2025-11-06 19:52:32 +0300

    v2.1

--- program2.py
+++ program2.py
@@ -2,7 +2,7 @@ if __name__ =='__main__':
 
     print('''
           -----------------------------------
-          Ogrenci bilgi sistemi v.1
+          | Ögrenci bilgi sistemi v.1       |
           -----------------------------------
           | Komut Listesi                   |
           -----------------------------------
@@ -12,8 +12,8 @@ if __name__ =='__main__':
           | listele | Ogrencileri listeler  |
           -----------------------------------
           ''')
-    ogrenci_adlari = ['Betul','Turkan','Simge']
-    ogrenci_soyadlari = ['Becerikli','Topkara','Dogan']
+    ogrenci_adlari = ['Betul','Furkan','Simge']
+    ogrenci_soyadlari = ['Becerikli','SAHİN','Dogan']
     ogrenci_numaralari = ['1234','1235','1236']
     
 
@@ -31,16 +31,13 @@ if __name__ =='__main__':
             print('------------------------------------------------------')
         elif komut == 'sil':
             print('------------------------------------------------------')
-            Ogrenci_numarasi = (input('Ogrenci sayisini giriniz:'))
-            
-
+            Ogrenci_numarasi = (input('Ogrenci numarasini giriniz:'))
             #index = -1
             #for i, numara in enumerate(ogrenci_numaralari):
             #    if numara == Ogrenci_numarasi:
             #        index = i
             try:
                 index = ogrenci_numaralari.index(Ogrenci_numarasi)
-
                 ogrenci_numaralari.pop(index)
                 ogrenci_adlari.pop(index)
                 ogrenci_soyadlari.pop(index)
@@ -59,13 +56,11 @@ if __name__ =='__main__':
 
             print('------------------------------------------------------')
         elif komut == 'listele':
-            print('------------------------------------------------------')
-            Ogrenci_sayisi = int(input('Ogrenci sayisini giriniz:'))
-            print('-' * 100)
-            print(f'|{' '*2}| {"Isim":<12} | {"Soyisim":<8} | {"Numara":<6} |')
-            print('-' * 100)
-            for sira in range(len(Ogrenci_sayisi)):
-                print(f'| {(sira+1):>2} | {ogrenci_adlari[sira]:<12} | {ogrenci_soyadlari[sira]:<8} | {ogrenci_numaralari[sira]:<4}')
+            print('-' * 54)
+            print(f'|{' '*3}| {"Isim":<15} | {"Soyisim":<15} | {"Numara":<10} |')
+            print('-' * 54)
+            for sira in range(len(ogrenci_adlari)):
+                print(f'| {(sira+1):^1} | {ogrenci_adlari[sira]:<15} | {ogrenci_soyadlari[sira]:<15} | {ogrenci_numaralari[sira]:<10} |')
                 
             print('------------------------------------------------------')
         else:
